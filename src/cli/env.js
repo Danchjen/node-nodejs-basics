08eafa1772bc1109c9bb558ed4a105a036e4a9a4
Danchjen <chjendanil@gmail.com>
2025-10-26 17:41:38 +0500

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+// env for testing: RSS_name=John RSS_role=Developer
+// command for testing:
+// RSS_name=John RSS_role=Developer node env.js
+
 const parseEnv = () => {
-  // Write your code here
+  const rssKeys = Object.keys(process.env).filter((key) =>
+    key.startsWith("RSS_")
+  );
+
+  console.log("ENV keys: ", rssKeys.join("; "));
 };
 
 parseEnv();
