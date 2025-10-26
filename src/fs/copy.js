5aa8c98513a8e3a02f35c64c4592506fcf1a5640
Danchjen <chjendanil@gmail.com>
2025-10-26 17:08:14 +0500

    refactor: simplify copy.js using fs.cp

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -4,28 +4,14 @@ const copy = async () => {
   const srcFolder = "files";
   const destFolder = "files_copy";
 
-  // Check if source folder does not exist and throw an error if so
   fs.access(srcFolder, fs.constants.F_OK, (err) => {
     if (err) throw new Error("FS operation failed");
 
-    // Check if destination folder exists and throw an error if so
     fs.access(destFolder, fs.constants.F_OK, (err2) => {
       if (!err2) throw new Error("FS operation failed");
 
-      // Create destFolder
-      // @destFolder = "files_copy"
-      fs.mkdir(destFolder, (err3) => {
-        // Collect full file names
-        fs.readdir(srcFolder, (err4, files) => {
-          // Copy each file
-          files.forEach((file) => {
-            fs.copyFile(
-              `${srcFolder}/${file}`,
-              `${destFolder}/${file}`,
-              (err5) => {}
-            );
-          });
-        });
+      fs.cp(srcFolder, destFolder, { recursive: true }, (err3) => {
+        if (err3) throw new Error("FS operation failed");
       });
     });
   });
